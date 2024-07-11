3e73652bdc387897d27fdc10cbdee055dbdbf996
frederic-morales <tocomorales30@gmail.com>
2024-07-11 15:52:32 -0600

    Añadiendo funcionalidad al header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  content: boolean = true;
+  displayContent() {
+    this.content = this.content ? false : true;
+  }
+}
+ 
